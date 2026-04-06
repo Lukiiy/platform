1bf645734ed69a1642d11960f79c76db0d5f3c1e
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-04-06 10:51:28 -0300

    fix thread_in lagging dialoguer
    * Ok. No yeah, ok.
    * Removed thread_in

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use colored::Colorize;
 use dialoguer::Confirm;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::thread;
@@ -33,12 +33,11 @@ pub fn run_server(entry: &ServerEntry, jar_path: &PathBuf) -> Result<()> {
     let java = entry.java_path.as_deref().unwrap_or(&config.app.java_path);
 
     let mut process = Command::new(java).args(&jvm).current_dir(&entry.path)
-        .stdin(Stdio::piped()).stdout(Stdio::piped()).stderr(Stdio::piped())
+        .stdin(Stdio::inherit()).stdout(Stdio::piped()).stderr(Stdio::piped())
         .spawn().context("Failed to launch Java... Is it installed?")?;
 
     let stdout = process.stdout.take().unwrap();
     let stderr = process.stderr.take().unwrap();
-    let mut stdin = process.stdin.take().unwrap();
 
     let thread_out = thread::spawn(move || {
         for line in BufReader::new(stdout).lines() {
@@ -52,28 +51,12 @@ pub fn run_server(entry: &ServerEntry, jar_path: &PathBuf) -> Result<()> {
         }
     });
 
-    let thread_in = thread::spawn(move || {
-        for line in std::io::stdin().lock().lines() {
-            match line {
-                Ok(l) => {
-                    if stdin.write_all(format!("{l}\n").as_bytes()).is_err() { break; }
-
-                    let _ = stdin.flush();
-                }
-
-                Err(_) => break
-            }
-        }
-    });
-
     let _ = process.wait();
 
     println!("{}", "Server process ended.".dimmed());
 
     let _ = thread_out.join();
     let _ = thread_error.join();
-
-    drop(thread_in);
 
     Ok(())
 }
